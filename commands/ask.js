401f901c21f15f0a088cbab856be7ff8849273fd
miorafanantenana12 <166400877+miorafanantenana12@users.noreply.github.com>
2024-06-03 21:58:53 +0300

    Create ask.js

---
+++ commands/ask.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+
+const Prefixes = [
+    'Ia',
+    'chi ',
+    'capucine',
+    'ask',
+    'edge',
+    'miror',
+];
+
+module.exports = {
+    name: 'ask',
+    description: 'AI command',
+    author: 'Bruno',
+    aliases: ['capucine'],
+    category: 'ai',
+    cooldown: 3,
+    nashPrefix: false,
+    execute: async (api, event, args) => {
+        const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+        if (!prefix) {
+            return; // Invalid prefix, ignore the command
+        }
+        const prompt = event.body.substring(prefix.length).trim();
+
+        if (!prompt) {
+            api.sendMessage("🎯 Hey mes amours, je suis à votre disposition. Quelle est votre question❓", event.threadID, event.messageID);
+            return;
+        }
+
+        api.sendMessage("Processing your request...", event.threadID, event.messageID);
+
+        try {
+            const { data } = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+            const answer = data.answer;
+
+            api.sendMessage(`💖 CAPUCINE \n______________________\n${answer}\n_______________________ \n🎯 MIROR EDGE 🎯`, event.threadID, event.messageID);
+        } catch (error) {
+            console.error("Error:", error.message);
+            api.sendMessage("An error occurred while processing your request.", event.threadID, event.messageID);
+        }
+    },
+};
